bbc4ef3035bb6f4e736463c3526c00b69c34b4a5
Anas Ullah Khan Niazi. <a.u.k.niazi15@gmail.com>
2026-05-15 10:33:07 +0400

    Add files via upload
    Product Wise Sum Without HashMap

---
+++ AssignNo8_1.java
@@ -0,0 +1,93 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class AssignNo8_1 {
+        public static void main(String[] args)
+        {
+            File f1 = new File("C:/Users/CZ 3/OneDrive/Desktop/Java Programs By Anas/SalesEVESession.txt");
+    
+            try
+            {
+                Scanner inp = new Scanner(f1);
+    
+                // Header Line
+                String dl = inp.nextLine();
+                System.out.println(dl);
+    
+                // Arrays
+                String[] productNames = new String[100];
+                double[] totalSales = new double[100];
+    
+                int count = 0;
+    
+                double grandTotal = 0;
+    
+                while (inp.hasNextLine())
+                {
+                    dl = inp.nextLine();
+    
+                    System.out.println(dl);
+    
+                    String[] parts = dl.trim().split("\\s{2,}");
+    
+                    String prodName = parts[1];
+    
+                    double uPrice = Double.parseDouble(parts[2]);
+    
+                    double qty = Double.parseDouble(parts[3]);
+    
+                    double amt = uPrice * qty;
+    
+                    // Grand Total
+                    grandTotal += amt;
+    
+                    boolean found = false;
+    
+                    // Check if product already exists
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (productNames[i].equals(prodName))
+                        {
+                            totalSales[i] += amt;
+                            found = true;
+                            break;
+                        }
+                    }
+    
+                    // If product not found
+                    if (found == false)
+                    {
+                        productNames[count] = prodName;
+                        totalSales[count] = amt;
+                        count++;
+                    }
+                }
+    
+                inp.close();
+    
+                // Product Wise Total
+                System.out.println("\n------ PRODUCT WISE TOTAL SALES ------");
+    
+                for (int i = 0; i < count; i++)
+                {
+                    System.out.println(productNames[i] + " --- " + totalSales[i]);
+                }
+    
+                System.out.println("--------------------------------------");
+    
+                // Grand Total
+                System.out.println("GRAND TOTAL = " + grandTotal);
+            }
+    
+            catch (FileNotFoundException e)
+            {
+                System.out.println("File not found");
+            }
+    
+            catch (Exception e)
+            {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
